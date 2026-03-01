db4e61c2f77fc9f7602bba8a41732db79f4b027f
alwanfikri <66763569+alwanfikri@users.noreply.github.com>
2026-03-01 14:23:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const targetDate = new Date("July 12, 2026 08:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  document.getElementById("countdown").innerHTML = days + " Hari Lagi";
+}, 1000);
