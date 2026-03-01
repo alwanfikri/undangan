a1adf6a147da8d8e13327535fcfc77e8a2744fa0
alwanfikri <66763569+alwanfikri@users.noreply.github.com>
2026-03-01 14:39:02 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const params = new URLSearchParams(window.location.search);
+const guestName = params.get("to");
+
+if (guestName) {
+  document.getElementById("guest").innerHTML =
+    "Kepada Yth. " + guestName;
+}
+
 const targetDate = new Date("July 12, 2026 08:00:00").getTime();
 
 setInterval(() => {
   const now = new Date().getTime();
   const distance = targetDate - now;
-
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  document.getElementById("countdown").innerHTML = days + " Hari Lagi";
+  document.getElementById("countdown").innerHTML =
+    days + " Hari Lagi";
 }, 1000);
